80560fb073a9bce7e70b000fe9bdb2e0fd95adb0
SeongJongHo <whdgh9595@gmail.com>
2025-05-20 00:00:43 +0900

    [CHORE] Redis 구현체 Lettuce 설정 추가

--- api/src/main/java/com/jongho/common/config/RedisConfig.java
+++ api/src/main/java/com/jongho/common/config/RedisConfig.java
@@ -1,13 +1,22 @@
 package com.jongho.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 
 @Configuration
 public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
     @Bean
     public RedisMessageListenerContainer redisMessageListener(
             RedisConnectionFactory connectionFactory) {
@@ -16,6 +25,14 @@ public class RedisConfig {
 
         return container;
     }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(redisHost,
+            redisPort);
+        return new LettuceConnectionFactory(configuration);
+    }
+
     @Bean
     public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
 
