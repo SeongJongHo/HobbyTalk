2f3a3b6f267d361d6fc4ca3dbc0166414686e459
SeongJongHo <whdgh9595@gmail.com>
2025-05-22 16:51:49 +0900

    [ADD] 오픈챗룸 ids 가져오는 리더 클래스 작성

---
+++ api/src/main/java/com/jongho/openChat/dao/step/OpenChatRoomIdsItemReaderImpl.java
@@ -0,0 +1,38 @@
+package com.jongho.openChat.dao.step;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.data.redis.core.Cursor;
+import org.springframework.data.redis.core.ScanOptions;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class OpenChatRoomIdsItemReaderImpl implements ItemReader<String> {
+    private final StringRedisTemplate stringRedisTemplate;
+    private final static String PATTERN = "chatRooms:*:chats";
+    private final static int BATCH_SIZE = 2000;
+    private Cursor<String> cursor;
+
+    public void reset() {
+        if (this.cursor != null && !cursor.isClosed()) {
+            cursor.close();
+        }
+        ScanOptions options = ScanOptions.scanOptions()
+            .match(PATTERN)
+            .count(BATCH_SIZE)
+            .build();
+        this.cursor = stringRedisTemplate.scan(options);
+    }
+
+    @Override
+    public String read() {
+        String result = null;
+        if (cursor != null && cursor.hasNext()) {
+            result = cursor.next();
+        }
+
+        return result;
+    }
+}
